72342a47087957dc6076d4ea09678360366d0a36
jasonycliu <jasonycliu@gmail.com>
2015-10-28 13:27:20 +0800

    迭代2-实现读写

--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -1,57 +1,34 @@
-# -*- coding:utf-8 -*- 
-#!/usr/bin/env python
+# -*- conding:utf-8 -*-
+from Tkinter import *
+from math import *
 
-'''
-本周整体任务概述:
+def evaluate(event):
+    inputtext = str(eval(entry.get()))
+    outputcontent = readfile() + inputtext + "\n"
+    res.configure(text = outputcontent)
+    writefile(outputcontent)
+    entry.delete(0, END)
 
-在上周开发基础上, 完成 极简交互式日记的桌面版本
-需求如下:
-每次运行时合理的打印出过往的所有日记
-一次接收输入一行日记
-保存为本地文件
-时限: 1wd4~2wd3
-发布: 发布到各自仓库的 _src/om2py2w/2wex0/ 目录中
+def readfile():
+    filetext = open('mydiary.txt','r').read()
+    return filetext
 
-脚本: main.py
-日记文件:mydiary.txt
+def writefile(outputcontent):
+    fileobject = open('mydiary.txt','w+')
+    fileobject.write(outputcontent)
+    fileobject.close()
 
-'''
+w = Tk()
+Label(w, text="my diary").pack()
+entry = Entry(w)
+entry.bind("<Return>", evaluate)
+entry.pack()
 
-import time
-from Tkinter import * 
+res = Label(w)
+res.pack()
+res.configure(text = readfile())
 
-class App(Frame):
-	
-	#初始化frame
-	def __init__(self,master=None):
-		Frame.__init__(self,master)
-		self.pack()
-		self.createWidegets()
+quit = Button(w,text="QUIT",fg="red",command=w.quit)
+quit.pack(side="left")
 
-	#创建组件
-	def createWidegets(self):
-
-		self.label = Label(self,width='50',text="->(⊙o⊙)<-")
-		self.label.pack()
-
-		self.entry = Entry(self,width='50')
-		self.entry.insert(0,'主人呢?!快来记日记啊!!')
-		self.entry.pack()
-
-		self.button = Button(self,text="QUIT",command=self.quit,anchor=E)
-		self.button.pack(side=LEFT)
-
-		#从本地读取文件
-		self.readfile()
-
-	def readfile(self):
-		filetext = open('mydiary.txt','r').read()
-		print filetext
-		self.display = Label(self,bg='gray',text=filetext)
-		self.display.pack()
-
-root = Tk()
-app = App(master=root)
-
-app.mainloop()
-root.destroy()
+w.mainloop()
