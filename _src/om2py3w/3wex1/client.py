22894de9d38bec2b3b55dc1fba8bc5db27b88eac
Jasonycliu <jasonycliu@gmail.com>
2015-11-15 15:38:11 +0800

    sina

--- _src/om2py3w/3wex1/client.py
+++ _src/om2py3w/3wex1/client.py
@@ -2,32 +2,23 @@
 # -*- coding: utf-8 -*-
 import socket
 
-HOST = '127.0.0.1'
-PORT = 9999
-
 #创建一个客户端的基于TCP 协议的socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 #建立连接
-#s.connect(HOST,PORT)
-s.connect((HOST,PORT))
-
-#接收服务的欢迎信息
-print s.recv(1024)
+s.connect(('127.0.0.1',9999))
 
 #运行程序打印历史日记
-s.send('r')
-print s.recv(1024)
+#s.send('r')
+#print s.recv(1024)
 
 while True:
 	inputtext = raw_input("请输入日记->")
-	#发送消息
-	s.send(inputtext)
-	#输入q退出客户端
 	if(inputtext == 'q'):
 		break
-	else:
-		#接收消息
-		data = s.recv(1024)
-		print data
+	#发送消息
+	s.send(inputtext)
+	#接收消息
+	data = s.recv(1024)
+	print data
 s.close()
--- _src/om2py3w/3wex1/server.py
+++ _src/om2py3w/3wex1/server.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import socket
-import time
-import threading
-
-HOST = '127.0.0.1'
-PORT = 9999
-
-#创建TCP socket
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-#绑定端口
-s.bind((HOST,PORT))
-
-#监听端口,并设置等待连接的最大数为5
-s.listen(5)
-
-print '等待客户端输入日记...'
-
-outputtext = ""
+import socket,time,threading
 
 #读取历史日记文件
 def read():
@@ -31,34 +13,43 @@ def write(outputtext):
 		f.write(outputtext)
 
 def tcp(sock,addr):
+
+	outputtext = ""
 	print '接收到来自 %s:%s...' % addr
-	sock.send("欢迎使用日记本...")
-	
+
 	while True:
-		data = s.recv(1024)
-		time.sleep()
+		data = sock.recv(1024)
+		
 		#当获取的指令为r时,客户端同步数据
 		if(data == 'r'):
 			history = read()
-			sock.send(history,addr)
+			sock.send(history)
 
 		#当指令为h时,客户端打印当前帮助信息
 		elif(data == 'h'):
 			helptext = '\n'.join(['输入[q]退出程序','输入[r]同步历史笔记','输入[h]打印当前日记'])
-			sock.send(helptext,addr)
+			sock.send(helptext)
 			
 		else:
 			outputtext += '\n' + data
 			#写数据到文件中
 			write(outputtext)
-			sock.send(outputtext,addr)
-	sock.close
-	print '来自 %s:%s 请求已经关闭.' % addr
+			if(data != 'q'):
+				sock.send(outputtext)
+
+#创建TCP socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#绑定端口
+s.bind(('127.0.0.1',9999))
+
+#监听端口,并设置等待连接的最大数为5
+s.listen(5)
 
 #服务端通过一个永久循环来接收消息
 while True:
 	#accept 接收一个客户的连接并返回
 	sock,addr = s.accept()
 	# 创建新线程来处理TCP连接: 否则在处理的过程中无法处理其他的客户端请求	
-    	t = threading.Thread(target=tcp, args=(sock, addr))
-    	t.start()
+	t = threading.Thread(target=tcp, args=(sock, addr))
+	t.start
